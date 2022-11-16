4fdfc2a19d17e1c8d64ea9624473afb41d23a4f9
joaomarcuscardoso <joao.cardoso@tikiweb.com.br>
2022-11-16 00:04:06 -0300

    init project, model etc...

---
+++ src/app/Order.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from "mongoose";
+
+export const Order = model("Order", new Schema({
+  table: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["WAITING", "IN_PRODUCTION", "DONE"],
+  }
+}));
